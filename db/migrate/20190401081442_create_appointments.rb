bb263995266aed1c885251e8d7c3d74b3086ce54
Derek Johnson <wars2cool@gmail.com>
2019-04-01 17:15:22 +0900

    Added Appointment Model

---
+++ db/migrate/20190401081442_create_appointments.rb
@@ -0,0 +1,11 @@
+class CreateAppointments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :appointments do |t|
+      t.references :user, foreign_key: true
+      t.references :lesson, foreign_key: true
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+  end
+end
