f5785367b3483863937ccdb13c22546fe815bfc3
nami izarra <nami.izarra@gmail.com>
2019-04-02 11:28:36 +0900

    Add dotenv - Protect my secret data in .env file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+CLOUDINARY_URL=cloudinary://665534534777238:GgLnrxsIiULI3BAFjiG9rnEYXFs@dpttkxync
