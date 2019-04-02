aa1915178eaee830c1581c582f334eb42004a0f8
nami izarra <nami.izarra@gmail.com>
2019-04-02 17:05:24 +0900

    created lessons cards

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,7 +8,7 @@ class Lesson < ApplicationRecord
   validates :postal_code, presence: true
 
   def nice_date
-    date.strftime("%B %d, %a") if date
+    date.strftime("%b %d, %a") if date
   end
 end
 
