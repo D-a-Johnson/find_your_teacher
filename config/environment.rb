a808671cc2a7857ec746b7c9351e2e3b87a0333b
nami izarra <nami.izarra@gmail.com>
2019-04-02 11:59:26 +0900

    fixed env file

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-CLOUDINARY_URL=cloudinary://665534534777238:GgLnrxsIiULI3BAFjiG9rnEYXFs@dpttkxync
