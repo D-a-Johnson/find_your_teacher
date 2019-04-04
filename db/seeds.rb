5d583a9f5d66a23369f3781838b4dcfbc6853bfd
Derek Johnson <wars2cool@gmail.com>
2019-04-04 15:01:28 +0900

    Updated seed dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ User.create!(
     email: "mcdonalds@everyday.omg",
     phone_number: "075-342-5185",
     remote_avatar_url: "https://avatars1.githubusercontent.com/u/43994166?s=460&v=4",
-    username: "McDonalds",
+    username: "Hamburgler",
     password:"123456"
 
   )
@@ -25,7 +25,7 @@ User.create!(
     email: "cocoichi@4ever.com",
     phone_number: "420-6969-6969",
     remote_avatar_url: "https://avatars1.githubusercontent.com/u/42726237?s=400&v=4",
-    username: "Russian_Spy",
+    username: "Russian_Spy_Putin<3",
     password:"123456"
   )
 
@@ -35,7 +35,7 @@ User.create!(
     email: "emoji-specialist@art.net",
     phone_number: "432-659-1953",
     remote_avatar_url: "https://avatars1.githubusercontent.com/u/46549190?s=400&v=4",
-    username: "Pao",
+    username: "Pooh Bear",
     password:"123456"
   )
 
@@ -84,7 +84,7 @@ Lesson.create!(
     detail: "I can teach you how to make user icons for fun! I am very good at it so I hope you look at my portfolio and come! :D",
     city: "Tokyo",
     postal_code: "Aoyama Gakuin",
-    date: DateTime.new(2009,1,12,10,35)
+    date: DateTime.new(2019,2,12,10,35)
     )
 
 Lesson.create!(
@@ -95,7 +95,7 @@ Lesson.create!(
     detail: "Latin Lessons! Please come if you can read the following: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eos, porro veniam sequi officiis fuga, beatae quasi quisquam. Natus adipisci et omnis, illum magni quae dolorem. Fugiat accusantium distinctio, quibusdam!",
     city: "Sapporo",
     postal_code: "0",
-    date: DateTime.new(2009,9,14,8,30)
+    date: DateTime.new(2019,9,14,8,30)
     )
 
 Lesson.create!(
@@ -117,7 +117,7 @@ Lesson.create!(
     detail: "My name is Alex and I am the best cook and popular founder of ChowTime(TM)! I am willing to run you through the basics of cooking just about anything from Gulash to Ramen! I specialize in Russian and Japanese dishes. You can find me in any bar around Nagoya station!",
     city: "Nagoya",
     postal_code: "450-0001",
-    date: DateTime.new(2020,5,20,16,30)
+    date: DateTime.new(2019,5,20,16,30)
   )
 
 Lesson.create!(
@@ -128,7 +128,7 @@ Lesson.create!(
     detail: "You may know me for my great art but this is a photography lesson! Making art is all the same stuff so I am also an expert in contempory photography!",
     city: "Osaka",
     postal_code: "530-6003",
-    date: DateTime.new(2009,9,14,8,30)
+    date: DateTime.new(2019,9,14,8,30)
     )
 
 Lesson.create!(
@@ -139,7 +139,7 @@ Lesson.create!(
     detail: "My name is Alex and I am offering English tutoring. I have the best English, believe me, lots of experts say so! Please disregard the Russian accent",
     city: "Pyeongyang",
     postal_code: "0",
-    date: DateTime.new(2018,7,27,18,45)
+    date: DateTime.new(2019,7,27,18,45)
   )
 
 Lesson.create!(
@@ -172,5 +172,5 @@ Lesson.create!(
     detail: "Painting lessons here! I specialize in finger painting only the greatest of pictures! It is also a great snack so please come!",
     city: "Busan",
     postal_code: "600-011",
-    date: DateTime.new(2014,11,14,16,0)
+    date: DateTime.new(2019,11,14,16,0)
   )
