e63ca4d0664818868460253e75b5abb4e0e77d04
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-04 15:01:07 +0900

    review controller finished, created review pundit policy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,15 +4,25 @@ class ReviewsController < ApplicationController
   end
 
   def create
-
+    @review = Review.new
+    @review.user = current_user
+    @lesson = Lesson.find(params[:lesson_id])
+    @review.lesson = @lesson
+    if @review.save
+      redirect_to lesson_path(@lesson)
+    else
+      render "lessons/show"
+    end
   end
 
   def destroy
+    @review.destroy
+    redirect_to lesson_path(@lesson)
   end
 
   private
 
   def review_params
-
+    params.require(:review).permit(:user_id, :lesson_id, :rating, :content)
   end
 end
