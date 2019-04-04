23a0d59677acb420f33a85cc44d4002ceb606ec1
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-04 12:26:21 +0900

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lessons, only: [:index, :new, :create, :show, :destroy] do
     resources :appointments, only: [:create]
+    resources :reviews, only: [:create, :index, :destroy]
   end
 
   resources :users, only: [:new, :create, :show]
