2563b66f05babdf19ea34c3b10d5d2a1db8d2fe1
Aya Kashihara <aya.kashihara@gmail.com>
2019-04-04 18:01:11 +0900

    create lesson form updated.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    @user = current_user
     authorize @user
 
     @lesson = Lesson.new
     authorize @lesson
 
-    @lessons = Lesson.where(user_id: params[:id])
-    @appointments = Appointment.where(user_id: params[:id])
+    # @lessons = Lesson.where(user_id: params[:id])
+    @lessons = current_user.lessons
+    # @appointments = Appointment.where(user_id: params[:id])
+    @appointments = current_user.appointments
   end
 
   private
