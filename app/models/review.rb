5987c7f747cf4af55d6f3b7ef21b53edbcb6a422
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-04-05 11:13:40 +0900

    fix collection 1..5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
-  validates :rating, presence: true, inclusion: { in: 0..5 }
+  validates :rating, presence: true, inclusion: { in: 1..5 }
 end
